78effb2e9265e6da95ddece8727f98ed29225a45
bruce <MACHD@bruce-m1.local>
2025-01-23 18:14:55 -0500

    clean resume site

--- weather/src/area/Area.js
+++
@@ -1,9 +0,0 @@
-/* globals angular */
-(function() {
-  'use strict';
-
-  // Prepare the 'area' module for subsequent registration of controllers and delegates
-  //noinspection JSUnresolvedFunction
-  angular.module('area', ['ngMaterial']);
-
-})();
